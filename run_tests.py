7e8df83044f48b7a222ad276cf6fa995a37623b6
Victor Batista <victor.guedes.08@gmail.com>
2025-05-02 12:04:33 -0300

    5. Classe TestSuite

--- run_tests.py
+++ run_tests.py
@@ -1,30 +1,26 @@
 from test_case_test import TestCaseTest
+from test_suite_test import TestSuiteTest
 from test_result import TestResult
+from test_suite import TestSuite
 
 result = TestResult()
+suite = TestSuite()
 
-test = TestCaseTest('test_result_success_run')
-test.run(result)
+# Adicionando testes de TestCaseTest
+suite.add_test(TestCaseTest('test_result_success_run'))
+suite.add_test(TestCaseTest('test_result_failure_run'))
+suite.add_test(TestCaseTest('test_result_error_run'))
+suite.add_test(TestCaseTest('test_result_multiple_run'))
+suite.add_test(TestCaseTest('test_was_set_up'))
+suite.add_test(TestCaseTest('test_was_run'))
+suite.add_test(TestCaseTest('test_was_tear_down'))
+suite.add_test(TestCaseTest('test_template_method'))
 
-test = TestCaseTest('test_result_failure_run')
-test.run(result)
+# Adicionando testes de TestSuiteTest
+suite.add_test(TestSuiteTest('test_suite_size'))
+suite.add_test(TestSuiteTest('test_suite_success_run'))
+suite.add_test(TestSuiteTest('test_suite_multiple_run'))
 
-test = TestCaseTest('test_result_error_run')
-test.run(result)
-
-test = TestCaseTest('test_result_multiple_run')
-test.run(result)
-
-test = TestCaseTest('test_was_set_up')
-test.run(result)
-
-test = TestCaseTest('test_was_run')
-test.run(result)
-
-test = TestCaseTest('test_was_tear_down')
-test.run(result)
-
-test = TestCaseTest('test_template_method')
-test.run(result)
-
+# Executando a suíte de testes
+suite.run(result)
 print(result.summary())
