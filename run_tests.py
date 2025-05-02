9ae05ea4d79102a6b197b52968cb6a7087750d04
Victor Batista <victor.guedes.08@gmail.com>
2025-05-02 12:09:24 -0300

    6. Classes TestLoader e TestRunner

--- run_tests.py
+++ run_tests.py
@@ -1,26 +1,23 @@
+from test_loader import TestLoader
+from test_runner import TestRunner
 from test_case_test import TestCaseTest
 from test_suite_test import TestSuiteTest
-from test_result import TestResult
-from test_suite import TestSuite
+from test_loader_test import TestLoaderTest
+from test_suite import TestSuite  # Importando TestSuite
 
-result = TestResult()
+loader = TestLoader()
+runner = TestRunner()
+
+# Criando suítes de testes
+test_case_suite = loader.make_suite(TestCaseTest)
+test_suite_suite = loader.make_suite(TestSuiteTest)
+test_loader_suite = loader.make_suite(TestLoaderTest)
+
+# Adicionando todas as suítes em uma suíte principal
 suite = TestSuite()
+suite.add_test(test_case_suite)
+suite.add_test(test_suite_suite)
+suite.add_test(test_loader_suite)
 
-# Adicionando testes de TestCaseTest
-suite.add_test(TestCaseTest('test_result_success_run'))
-suite.add_test(TestCaseTest('test_result_failure_run'))
-suite.add_test(TestCaseTest('test_result_error_run'))
-suite.add_test(TestCaseTest('test_result_multiple_run'))
-suite.add_test(TestCaseTest('test_was_set_up'))
-suite.add_test(TestCaseTest('test_was_run'))
-suite.add_test(TestCaseTest('test_was_tear_down'))
-suite.add_test(TestCaseTest('test_template_method'))
-
-# Adicionando testes de TestSuiteTest
-suite.add_test(TestSuiteTest('test_suite_size'))
-suite.add_test(TestSuiteTest('test_suite_success_run'))
-suite.add_test(TestSuiteTest('test_suite_multiple_run'))
-
-# Executando a suíte de testes
-suite.run(result)
-print(result.summary())
+# Executando os testes
+runner.run(suite)
